3ac6edc979aebdd5c46dcf245e54e132907d9e18
saeed-m-l <83838941+saeed-m-l@users.noreply.github.com>
2021-06-03 23:52:49 -0700

    Update DomesticAnimalEnum.java

--- DomesticAnimalEnum.java
+++ DomesticAnimalEnum.java
@@ -1,7 +1,7 @@
 public enum DomesticAnimalEnum {
-    GOOSE("egg",0,0,0,0),
-    COW("wool",0,0,0,0),
-    CHICKEN("egg",0,0,0,0);
+    GOOSE(Egg,0,0,0,0),
+    COW(Wool,0,0,0,0),
+    CHICKEN(Egg,0,0,0,0);
     private final String product;
     private final int speed;
     private final int buyPrice;
@@ -13,7 +13,7 @@ public enum DomesticAnimalEnum {
     private final int sellPrice;
     private final int size;
 
-    DomesticAnimalEnum(String product, int speed, int buyPrice, int sellPrice, int size) {
+    DomesticAnimalEnum(Product product, int speed, int buyPrice, int sellPrice, int size) {
         this.product = product;
         this.speed = speed;
         this.buyPrice = buyPrice;
