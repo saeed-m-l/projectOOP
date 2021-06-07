79838e553f9971b381926a60a648d4727ea84ee4
asal <73219545+asalroudbari@users.noreply.github.com>
2021-06-07 22:31:24 +0430

    Add files via upload

--- Product.java
+++ Product.java
@@ -1,15 +1,20 @@
-public abstract class Product {
+public enum Product {
+    BREAD(80, 4),
+    CLOTH(50, 2),
+    DRESS(100 , 4),
+    EGG(15, 1),
+    FEATHER(20 , 1),
+    FLOUR(40 , 2),
+    ICECREAM(120 , 4),
+    MILK1(25, 1);
+
     private String name;
     private int price;
     private int capacity;
     //int number ;
-    public Product(String name, int price, int capacity) {
-        this.name = name;
+    Product(int price, int capacity) {
         this.price = price;
         this.capacity = capacity;
-    }
-    public String getName() {
-        return name;
     }
     public int getPrice() {
         return price;
--- Shop.java
+++ Shop.java
@@ -1,15 +1,23 @@
-public abstract class Shop {
+package model;
+
+public enum Shop {
+    SEWINGMACHINE(Product.CLOTH, Product.DRESS, 1, 6, 400),
+    MILKSHOP(Product.MILK, Product.CARTONOFMILK, 1, 6, 400),
+    ICECREAMSHOP(Product.CARTONOFMILK, Product.ICECREAM, 1, 7, 550),
+    FLOURSHOP(Product.EGG, Product.FLOUR, 1, 4, 150),
+    CLOTHSHOP(Product.FEATHER, Product.CLOTH, 1, 5, 250);
+
     int buyPrice;
     int timeToFinish;
     Product input;
     Product output;
     int level;
-    int numberofProducts;
+    int numOfProducts;
     boolean isWorking = false;
     int upgradeCost;
     boolean isAvailable = false;
 
-    public Shop(Product input, Product output, int level, int timeToFinish , int buyPrice){
+    Shop(Product input, Product output, int level, int timeToFinish , int buyPrice){
         this.input = input ;
         this.output = output ;
         this.level =level ;
@@ -17,14 +25,14 @@ public abstract class Shop {
         this.buyPrice = buyPrice;
     }
 
-    public void buy (){
+    public void buy () {
         totalMoney-=buyPrice;
         isAvailable=true;
     }
     public void upgrade(int level){
         totalMoney-=upgradeCost;
         level++;
-        numberofProducts++;
+        numOfProducts++;
     }
     public void startWorking(){
         if (isWorking==false) {
